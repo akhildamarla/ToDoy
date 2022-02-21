38b177761bcc9fb21907c372d881600c014e2bc7
Akhil Damarla <akhildamarla@Akhils-MacBook-Pro.local>
2022-02-21 01:21:50 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+//Importing components
+import Form from './Components/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>ToDoy</h1>
+      </header>
+      <Form />
+
+      
+    </div>
+  );
+}
+
+export default App;
